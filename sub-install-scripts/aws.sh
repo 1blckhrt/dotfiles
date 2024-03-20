024aecae27198f4665cbe8b30474ea9f8abfb4fc
Parker W <143528734+1blckhrt@users.noreply.github.com>
2024-03-20 15:21:30 -0400

    Update aws.sh

--- sub-install-scripts/aws.sh
+++ sub-install-scripts/aws.sh
@@ -16,7 +16,7 @@ install_aws_cli() {
     if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         unzip awscliv2.zip
-        ./aws/install || {
+        sudo ./aws/install || {
             echo "[ERROR] Failed to install AWS CLI. Exiting..."
             exit 1
         }
