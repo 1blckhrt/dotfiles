9136d44227792b6ec478333cbee76a62bca9bec1
Parker W <143528734+1blckhrt@users.noreply.github.com>
2024-09-24 11:53:14 -0400

    fix hyprland dependencies to use latest version

--- linux_mint/scripts/hyprland.sh
+++ linux_mint/scripts/hyprland.sh
@@ -4,10 +4,6 @@ hyprland_install() {
   sudo nala update -y
   sudo nala upgrade -y
   pacstall -I xcb-util-errors hyprlang hyprcursor-bin hyprland-bin hyprpaper
-  wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.23_amd64.deb
-  wget http://mirrors.kernel.org/ubuntu/pool/universe/libz/libzip/libzip5_1.5.1-0ubuntu1_amd64.deb
-  sudo nala install ./libssl1.1_1.1.1f-1ubuntu2.23_amd64.deb
-  sudo nala install ./libzip5_1.5.1-0ubuntu1_amd64.deb
-  sudo nala install libtomlplusplus-dev
+  sudo nala install libssl-dev libzip-dev libtomlplusplus-dev
 }
 hyprland_install
