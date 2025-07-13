e0417c6e4963e02b1c9f33232415a2d32d767c44
1blckhrt <williams.1691@wright.edu>
2025-07-13 17:34:30 -0400

    fix tmux config

--- nix/programs/tmux/default.nix
+++ nix/programs/tmux/default.nix
@@ -14,12 +14,18 @@
 
     plugins = with pkgs.tmuxPlugins; [
       resurrect
+      sensible
+      yank
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60'
+        '';
+      }
     ];
 
     extraConfig = ''
-      # Load plugins
-      run-shell ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
-
       # Key bindings and settings
       setw -g mode-keys vi
       bind d detach
@@ -33,6 +39,26 @@
       bind k select-pane -U
       bind l select-pane -R
       bind S choose-session
+
+      # Theme configuration (manual nord-like theme)
+      set -g status-bg "#2E3440"
+      set -g status-fg "#D8DEE9"
+      set -g window-status-current-bg "#81A1C1"
+      set -g window-status-current-fg "#2E3440"
+      set -g pane-border-fg "#4C566A"
+      set -g pane-active-border-fg "#81A1C1"
+      set -g message-bg "#81A1C1"
+      set -g message-fg "#2E3440"
+
+      # Status bar configuration
+      set -g status-left-length 40
+      set -g status-right-length 80
+      set -g status-left "#[fg=#2E3440,bg=#81A1C1,bold] #S #[fg=#81A1C1,bg=#2E3440,nobold,nounderscore,noitalics]"
+      set -g status-right "#[fg=#4C566A,bg=#2E3440,nobold,nounderscore,noitalics]#[fg=#D8DEE9,bg=#4C566A] %Y-%m-%d  %H:%M #[fg=#81A1C1,bg=#4C566A,nobold,nounderscore,noitalics]#[fg=#2E3440,bg=#81A1C1,bold] #h "
+
+      # Window status format
+      set -g window-status-format "#[fg=#2E3440,bg=#4C566A,nobold,nounderscore,noitalics]#[default] #I  #W #[fg=#4C566A,bg=#2E3440,nobold,nounderscore,noitalics]"
+      set -g window-status-current-format "#[fg=#2E3440,bg=#81A1C1,nobold,nounderscore,noitalics]#[fg=#2E3440,bg=#81A1C1,bold] #I  #W #[fg=#81A1C1,bg=#2E3440,nobold,nounderscore,noitalics]"
 
       # Custom which-key menu
       bind-key Space display-menu -T "tmux keybindings" \
@@ -54,6 +80,11 @@
         "Save session (Resurrect)" "C-s" "run-shell '${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/scripts/save.sh'" \
         "Restore session (Resurrect)" "C-r" "run-shell '${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/scripts/restore.sh'" \
         "Show all keys" "?" list-keys
+
+      # Resurrect settings
+      set -g @resurrect-strategy-vim 'session'
+      set -g @resurrect-strategy-nvim 'session'
+      set -g @resurrect-capture-pane-contents 'on'
     '';
   };
 }
