d2a1a93a2fdaa78c5daeeac85f3796e2730aa8a8
1blckhrt <williams.1691@wright.edu>
2025-07-14 19:32:17 -0400

    add tmux powerline

--- nix/programs/tmux/default.nix
+++ nix/programs/tmux/default.nix
@@ -16,6 +16,7 @@
       resurrect
       sensible
       yank
+      tmux-powerline
       {
         plugin = continuum;
         extraConfig = ''
@@ -41,24 +42,11 @@
       bind S choose-session
 
       # Theme configuration (manual nord-like theme)
-      set -g status-bg "#2E3440"
-      set -g status-fg "#D8DEE9"
-      set -g window-status-current-bg "#81A1C1"
-      set -g window-status-current-fg "#2E3440"
-      set -g pane-border-fg "#4C566A"
-      set -g pane-active-border-fg "#81A1C1"
-      set -g message-bg "#81A1C1"
-      set -g message-fg "#2E3440"
-
-      # Status bar configuration
-      set -g status-left-length 40
-      set -g status-right-length 80
-      set -g status-left "#[fg=#2E3440,bg=#81A1C1,bold] #S #[fg=#81A1C1,bg=#2E3440,nobold,nounderscore,noitalics]"
-      set -g status-right "#[fg=#4C566A,bg=#2E3440,nobold,nounderscore,noitalics]#[fg=#D8DEE9,bg=#4C566A] %Y-%m-%d  %H:%M #[fg=#81A1C1,bg=#4C566A,nobold,nounderscore,noitalics]#[fg=#2E3440,bg=#81A1C1,bold] #h "
-
-      # Window status format
-      set -g window-status-format "#[fg=#2E3440,bg=#4C566A,nobold,nounderscore,noitalics]#[default] #I  #W #[fg=#4C566A,bg=#2E3440,nobold,nounderscore,noitalics]"
-      set -g window-status-current-format "#[fg=#2E3440,bg=#81A1C1,nobold,nounderscore,noitalics]#[fg=#2E3440,bg=#81A1C1,bold] #I  #W #[fg=#81A1C1,bg=#2E3440,nobold,nounderscore,noitalics]"
+      set -g status-style bg="#2E3440",fg="#D8DEE9"
+      set -g window-status-current-style bg="#81A1C1",fg="#2E3440"
+      set -g pane-border-style fg="#4C566A"
+      set -g pane-active-border-style fg="#81A1C1"
+      set -g message-style bg="#81A1C1",fg="#2E3440"
 
       # Custom which-key menu
       bind-key Space display-menu -T "tmux keybindings" \
