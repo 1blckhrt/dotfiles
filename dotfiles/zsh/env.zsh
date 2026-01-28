36ab50edf1fa6f00a83656967ba18ba32012f205
1blckhrt <blckhrtxo@gmail.com>
2026-01-27 19:50:46 -0500

    chore: backup on 2026-01-27 19:50:46

--- dotfiles/zsh/env.zsh
+++ dotfiles/zsh/env.zsh
@@ -8,4 +8,15 @@ export VISUAL=$EDITOR
 
 export PATH=$HOME/.local/bin:$PATH
 
+export BUN_INSTALL="$HOME/.bun" 
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# pnpm
+export PNPM_HOME="/home/blckhrt/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
